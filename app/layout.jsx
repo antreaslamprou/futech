a65a45016bee3051cdb92b94315139e300627f60
Antreas Lamprou <antreaslamprou12@gmail.com>
2025-10-05 02:28:33 +0300

    Update fonts to be preloaded by next.js

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "styles/globals.css";
 import Navbar from 'components/Navbar';
 import ClientLayout from 'components/ClientLayout';
 import Head from "next/head";
+import localFont from 'next/font/local';
 
 export const metadata = {
   title: "Futech",
@@ -11,24 +12,24 @@ export const metadata = {
   },
 };
 
+const timeburner = localFont({
+  src: './fonts/timeburner.ttf',
+  variable: '--font-timeburner',
+  weight: '400',
+  display: 'swap',
+});
+
+const ballega = localFont({
+  src: './fonts/ballega.otf',
+  variable: '--font-ballega',
+  weight: '400',
+  display: 'swap',
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link
-          rel="preload"
-          href="/fonts/timeburner.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/ballega.otf"
-          as="font"
-          type="font/otf"
-          crossOrigin="anonymous"
-        />
         <link
           rel="preload"
           as="image"
@@ -54,7 +55,7 @@ export default function RootLayout({ children }) {
           type="image/webp"
         />
       </Head>
-      <body className="antialiased">
+      <body className={`antialiased ${timeburner.variable} ${ballega.variable}`}>
         <ClientLayout>
           <Navbar />
           <main>
